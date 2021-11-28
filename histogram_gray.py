e2db9e0b61f85981469e92849ec9421520828944
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-11-29 01:26:56 +0530

    histogram of grayscale image added

--- histogram_gray.py
+++ histogram_gray.py
@@ -1,9 +1,13 @@
 import cv2
+import matplotlib.pyplot as plt
 
-img = cv2.imread("./download.jpg")
+img = cv2.imread("./download.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Original", img)
 
-dst = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-cv2.imshow("Rotated by 90 degrees", dst)
-cv2.waitKey(0)
+dst = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+plt.hist(img.ravel(), 256, [0, 256])
+plt.title("Grayscale Histogram")
+plt.show()
+
 cv2.destroyAllWindows()
